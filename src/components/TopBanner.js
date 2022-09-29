cb82d915b0395b1a46c6e0b30f0a895ef40ccc14
taeseokyoung <xotjrud@gmail.com>
2022-09-29 09:08:23 +0900

    20220929

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -3,7 +3,7 @@ import { HiOutlineX } from "react-icons/hi";
 const TopBanner = () => {
     return (
         <section className="TopBanner gothic">
-            <div className="hInner">
+            <div className="inner">
                 <p className="">SS23 에메랄드 캡슐 컬렉션을 온라인 스토어에서 가장 먼저 만나보세요.</p>
                 <a href="#!">자세히 보기</a>
             </div>
